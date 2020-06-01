b44fd19caef7ca4b082ac344db2ecfcc16d5ea02
Mirko <mirko.longo.1987@gmail.com>
2020-06-01 18:05:07 +0200

    Ok

---
+++ resources/views/admin/pages/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item"><a href="#">Pages</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Create</li>
+        </ol>
+      </nav>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12">
+      <div class="row">
+        <div class="col-6">
+          <h2>Pages</h2>
+        </div>
+        <div class="col-12">
+          <a href="#">Inserisci una nuova pagina</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -1,6 +1,40 @@
 @extends('layouts.app')
-@section('content')
+@php
+  $pages = [
+    [
+      'id' => 1,
+      'category' => 1,
+      'title' => 'Lorem ipsum 1',
+      'tags' => [
+        1,
+        3,
+        5
+      ],
+    ],
+    [
+      'id' => 2,
+      'category' => 1,
+      'title' => 'Lorem ipsum 2 ',
+      'tags' => [
+        2,
+        7,
+        5
+      ],
+    ],
+    [
+      'id' => 2,
+      'category' => 1,
+      'title' => 'Lorem ipsum 3',
+      'tags' => [
+        2,
+        6,
+        4
+      ],
+    ]
+    ];
+@endphp
 
+@section('content')
   <div class="container">
     <div class="row">
       <div class="col-12">
@@ -19,7 +53,7 @@
             <h2>Pages</h2>
           </div>
           <div class="offset-3 col-3">
-            <a href="#">Crea una pagina</a>
+            <a href="{{route('admin.pages.create')}}">Crea una pagina</a>
           </div>
         </div>
         <table class="table table-dark">
@@ -33,18 +67,29 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Lorem ipsum</td>
-              <td>Miscellania</td>
-              <td>Tag 1, Tag 2, tag 3</td>
-              <td><a class="btn btn-primary" href="#">Visualizza</a></td>
-              <td><a class="btn btn-secondary" href="#">Modifica</a></td>
-              <td>
-                <form class="" action="index.html" method="post">
-                  <input class="btn btn-danger" type="submit" name="" value="Elimina">
-                </form>
-              </td>
+            @foreach ($pages as $key => $page)
+              <tr>
+                <td>{{$page['id']}}</td>
+                <td>{{$page['title']}}</td>
+                <td>{{$page['category']}}</td>
+                <td>
+                  @foreach ($page['tags'] as $key => $tag)
+                    {{$tag}}
+                    {{-- se NON e' l'ultimo metto la , --}}
+                    @if (!$loop->last)
+                      ,
+                    @endif
+                  @endforeach
+                </td>
+                <td><a class="btn btn-primary" href="#">Visualizza</a></td>
+                <td><a class="btn btn-secondary" href="#">Modifica</a></td>
+                <td>
+                  <form class="" action="index.html" method="post">
+                    <input class="btn btn-danger" type="submit" name="" value="Elimina">
+                  </form>
+                </td>
+              </tr>
+            @endforeach
           </tbody>
         </table>
       </div>
